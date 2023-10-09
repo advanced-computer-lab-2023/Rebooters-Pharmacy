cabcd443caabf7dc0c95351b9302c023e4eadb42
Maya <mayamokhtar2002@gmail.com>
2023-10-09 16:07:12 +0300

    allowed a guest to register as a patient and request to register as a pharmacist

--- backend/src/Models/guestModel.js
+++ backend/src/Models/guestModel.js
@@ -1,9 +1,64 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const GuestSchema = new Schema(
-    {},
-    {timestamps : true});
+   
 
-const Guest = mongoose.model('Guest', GuestSchema);
-module.exports = Guest;   
+const emergencyContactSchema = new mongoose.Schema({
+    fullName: {
+      type: String,
+      required: true,
+    },
+    mobileNumber: {
+      type: String,
+      required: true,
+    },
+    relation: {
+      type: String,
+      required: true,
+    },
+  });
+  
+  const guestSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    dateOfBirth: {
+      type: Date,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    mobileNumber: {
+      type: String,
+      required: true,
+    },
+    emergencyContact: {
+      type: emergencyContactSchema,
+      required: true,
+    },
+  });
+  
+  const Guest = mongoose.model('Guest', guestSchema);
+  
+  module.exports = {
+ 
+     Guest
+  };
+  
